0476b39187d3d276cceee4a9ec10a7a95467f135
divine <ogochukwudivine222@gmail.com>
2025-01-24 10:46:52 +0100

    sign up part completed

--- djangochat/views.py
+++ djangochat/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import login
 
 def frontpage(request):
     return render(request, "djangochat/frontpage.html")
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()  
+            login(request, user)  
+            return redirect('frontpage')  
+    else:
+        form = SignUpForm()
+
+   
+    return render(request, 'djangochat/signup.html', {'form': form})
